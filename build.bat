0c450c4965eeb32397d9ff50766f9a556571c66d
Oskar Dudycz <o.dudycz@lgbs.pl>
2015-12-02 21:42:56 +0100

    Added build for file for MyGet

---
+++ build.bat
@@ -0,0 +1,18 @@
+@echo Off
+set config=%1
+if "%config%" == "" (
+   set config=Release
+)
+ 
+set version=1.0.0
+if not "%PackageVersion%" == "" (
+   set version=%PackageVersion%
+)
+
+set nuget=
+if "%nuget%" == "" (
+	set nuget=nuget
+)
+
+
+%nuget% pack "Knockout.ComponentByNamingConventionLoader.nuspec" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
