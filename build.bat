e7b6a07b7e23733a518d3760d9807d324f1a8271
Oskar Dudycz <o.dudycz@lgbs.pl>
2015-12-02 22:16:20 +0100

    MyGet build fix

--- build.bat
+++ build.bat
@@ -1,17 +1,1 @@
-@echo Off
-set config=%1
-if "%config%" == "" (
-   set config=Release
-)
- 
-set version=1.0.0
-if not "%PackageVersion%" == "" (
-   set version=%PackageVersion%
-)
-
-set nuget=
-if "%nuget%" == "" (
-	set nuget=nuget
-)
-
-%nuget% pack "Knockout.ComponentByNamingConventionLoader.nuspec" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%" -OutputDirectory $buildArtifactsDirectory
+nuget.exe pack "{0}" -OutputDirectory "{1}" -Prop Configuration={2} -NoPackageAnalysis -Symbols {3} -Version {4}
