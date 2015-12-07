ae7908b98d3f5e4544579c359feca384fe1867ec
Oskar Dudycz <o.dudycz@lgbs.pl>
2015-12-07 20:40:44 +0100

    Added missing method $.cachedScript

--- src/content/Scripts/ComponentByNamingConventionLoader.js
+++ src/content/Scripts/ComponentByNamingConventionLoader.js
@@ -131,3 +131,17 @@ ko.components.loaders.unshift((function () {
         loadViewModel: loadViewModel
     };
 })());
+
+jQuery.cachedScript = function (url, options) {
+
+    // Allow user to set any option except for dataType, cache, and url
+    options = $.extend(options || {}, {
+        dataType: "script",
+        cache: true,
+        url: url
+    });
+
+    // Use $.ajax() since it is more flexible than $.getScript
+    // Return the jqXHR object so we can chain callbacks
+    return jQuery.ajax(options);
+};
